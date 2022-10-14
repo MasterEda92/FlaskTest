cc67b1722d21c0d9da976e8794848f656d3d97b8
Stefan Eder <darkmaster.eder@gmail.com>
2022-10-14 13:09:56 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# Press the green button in the gutter to run the script.
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    items = ["Apfel", "Birne", "Banane"]
+
+    out = render_template("start.html", name="Stefan Eder", items=items)
+    return out
