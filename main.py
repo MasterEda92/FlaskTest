d1f84aa381e0b0d42de69cf46b2361b62a8be00f
Stefan Eder <darkmaster.eder@gmail.com>
2022-10-17 17:55:29 +0200

    add page currency

--- main.py
+++ main.py
@@ -33,3 +33,25 @@ def test():
     name = request.args.get("name")
     age = request.args.get("age")
     return render_template("test.html", name=name, age=age)
+
+
+@app.route("/currency")
+def currency():
+    currency1 = request.args.get("currency1", "EUR")
+    currency2 = request.args.get("currency2", "EUR")
+    rate = float(request.args.get("rate", "1.0"))
+
+    table1 = []
+    for x in range(1, 50):
+        table1.append((x, round(x * rate, 2)))
+
+    table2 = []
+    for x in range(1, 50):
+        table2.append((x, round(x / rate, 2)))
+
+    return render_template("currency.html",
+                           currency1=currency1,
+                           currency2=currency2,
+                           rate=rate,
+                           table1=table1,
+                           table2=table2)
